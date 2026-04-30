b9ce05383e091a45c7093b230e84ebcf608137db
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2026-04-29 18:10:28 -0600

    Simplified rope

--- src/rawformer/rope.py
+++ src/rawformer/rope.py
@@ -13,50 +13,46 @@ class RoPE1D(nn.Module):
         self.rotary_dim = rotary_dim
 
         init_base_theta = torch.tensor(init_theta, dtype=torch.float32)
-        self.log_base_theta = nn.Parameter(init_base_theta.log())
+        self.log_base = nn.Parameter(init_base_theta.log())
 
-    def build_rot_mat(
+    def build_cache(
         self, length: int
     ) -> tuple[Float[Tensor, "len rot_dim"], Float[Tensor, "len rot_dim"]]:
         # Builds the `rotation matrix`
 
-        base_theta = self.log_base_theta.exp()
-        num_rotary_freqs = self.rotary_dim // 2
+        half_dim = self.rotary_dim // 2
+        device, dtype = self.log_base.device, self.log_base.dtype
 
-        i = torch.arange(
-            0, num_rotary_freqs, device=base_theta.device, dtype=base_theta.dtype
-        )
-        theta = base_theta ** (-i / num_rotary_freqs)
-        positions = torch.arange(0, length)
-        freqs = torch.einsum(
-            "i,j -> ij", positions, theta
-        )  # (length, num_rotary_freqs)
+        i = torch.arange(half_dim, device=device, dtype=dtype)
+        theta = torch.exp(-i / half_dim * self.log_base)
+        positions = torch.arange(length, device=device, dtype=dtype)
+        freqs = torch.outer(positions, theta)  # (length, half_dim)
 
-        sin = torch.sin(freqs).repeat_interleave(2, dim=-1)  # (length, rotary_dim)
-        cos = torch.cos(freqs).repeat_interleave(2, dim=-1)  # (length, rotary_dim)
+        sin = freqs.sin().repeat_interleave(2, dim=-1)  # (length, half_dim)
+        cos = freqs.cos().repeat_interleave(2, dim=-1)  # (length, half_dim)
 
         return sin, cos
 
 
 def apply_rope(
     x: Float[Tensor, "b len dim"],
-    matrices: tuple[Float[Tensor, "len rot_dim"], Float[Tensor, "len rot_dim"]],
+    rope_cache: tuple[Float[Tensor, "len rot_dim"], Float[Tensor, "len rot_dim"]],
 ) -> Float[Tensor, "b len dim"]:
     # Uses efficient 2D rotation implementation from RoFormer paper eqn 34
 
     # Unpack input and validate
-    sin_mat, cos_mat = matrices
+    sin_mat, cos_mat = rope_cache
     rot_dim = sin_mat.shape[-1]
     assert rot_dim <= x.shape[-1], "rot_dim must be <= full dim"
 
-    # Split x by rot_dim
+    # Split x by rot_dim (only rotate top)
     x_top = x[..., :rot_dim]
     x_bottom = x[..., rot_dim:]
 
-    # Create flipped vector
-    x1 = x_top[..., ::2]
-    x2 = x_top[..., 1::2]
-    x_top_flipped = torch.stack((-x2, x1), dim=-1).reshape_as(x_top)
+    # Reorder into [-x_2, x_1, -x_4, x_3, ...]
+    x_even = x_top[..., ::2]
+    x_odd = x_top[..., 1::2]
+    x_top_flipped = torch.stack((-x_odd, x_even), dim=-1).reshape_as(x_top)
 
     # Compute rotations
     x_rot = (x_top * cos_mat) + (x_top_flipped * sin_mat)
